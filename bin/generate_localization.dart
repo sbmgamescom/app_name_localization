b11d2fd840c29697137a96aef8f3f8cdc88617fe
Shakutov Bekzat <shakutovbekzat@gmail.com>
2024-11-12 22:27:23 +0500

    add

---
+++ bin/generate_localization.dart
@@ -0,0 +1,6 @@
+import 'package:app_name_localization/app_name_localization.dart';
+
+void main() {
+  // Assuming you have a function like `generateLocalizedAppNames` defined in your main logic
+  generateLocalizedAppNames();
+}
--- lib/app_name_localization.dart
+++ lib/app_name_localization.dart
@@ -1,8 +1,10 @@
+// lib/app_name_localization.dart
+
 import 'dart:io';
 
 import 'package:yaml/yaml.dart';
 
-void main() {
+void generateLocalizedAppNames() {
   final configFile = File('app_names.yaml');
   if (!configFile.existsSync()) {
     print('Configuration file app_names.yaml not found.');
@@ -23,10 +25,26 @@ void main() {
     final file = File('${dir.path}/strings.xml');
     file.writeAsStringSync('''
 <resources>
-    <string name="app_name">$name</string>
+    <string name="app_name">${name ?? defaultName}</string>
 </resources>
     ''');
   });
+
+  // Update AndroidManifest.xml with android:label="@string/app_name"
+  final androidManifest = File('android/app/src/main/AndroidManifest.xml');
+  if (androidManifest.existsSync()) {
+    String manifestContent = androidManifest.readAsStringSync();
+    final labelPattern = RegExp(r'android:label="[^"]*"');
+    if (labelPattern.hasMatch(manifestContent)) {
+      manifestContent = manifestContent.replaceAll(
+          labelPattern, 'android:label="@string/app_name"');
+    }
+
+    androidManifest.writeAsStringSync(manifestContent);
+    print('AndroidManifest.xml updated with android:label="@string/app_name".');
+  } else {
+    print('AndroidManifest.xml not found.');
+  }
 
   // Generate iOS InfoPlist.strings files
   locales.forEach((locale, name) {
@@ -36,7 +54,7 @@ void main() {
     }
     final file = File('${dir.path}/InfoPlist.strings');
     file.writeAsStringSync('''
-"CFBundleDisplayName" = "$name";
+"CFBundleDisplayName" = "${name ?? defaultName}";
     ''');
   });
 
