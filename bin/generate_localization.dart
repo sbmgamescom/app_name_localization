d5bfcd271b6131c9cd37892ebfb20ee138185c18
Shakutov Bekzat <shakutovbekzat@gmail.com>
2024-11-12 22:27:26 +0500

    add

--- bin/generate_localization.dart
+++ bin/generate_localization.dart
@@ -1,6 +1,9 @@
 import 'package:app_name_localization/app_name_localization.dart';
 
+/// Command  dart run app_name_localization:generate_localization
 void main() {
   // Assuming you have a function like `generateLocalizedAppNames` defined in your main logic
+  print('Running app_name_localization inst!');
+
   generateLocalizedAppNames();
 }
--- lib/app_name_localization.dart
+++ lib/app_name_localization.dart
@@ -14,49 +14,55 @@ void generateLocalizedAppNames() {
   final config = loadYaml(configFile.readAsStringSync());
   final defaultName = config['default'] ?? 'My App';
   final locales = config['locales'] as Map;
+  final platforms = (config['platforms'] as YamlMap?)
+          ?.map((key, value) => MapEntry(key.toString(), value == true)) ??
+      {'android': true, 'ios': true};
 
-  // Generate Android strings.xml files
-  locales.forEach((locale, name) {
-    final folderName = locale == 'en' ? 'values' : 'values-$locale';
-    final dir = Directory('android/app/src/main/res/$folderName');
-    if (!dir.existsSync()) {
-      dir.createSync(recursive: true);
-    }
-    final file = File('${dir.path}/strings.xml');
-    file.writeAsStringSync('''
+  if (platforms['android'] == true) {
+    // Generate Android strings.xml files
+    locales.forEach((locale, name) {
+      final folderName = locale == 'en' ? 'values' : 'values-$locale';
+      final dir = Directory('android/app/src/main/res/$folderName');
+      if (!dir.existsSync()) {
+        dir.createSync(recursive: true);
+      }
+      final file = File('${dir.path}/strings.xml');
+      file.writeAsStringSync('''
 <resources>
     <string name="app_name">${name ?? defaultName}</string>
 </resources>
     ''');
-  });
+    });
 
-  // Update AndroidManifest.xml with android:label="@string/app_name"
-  final androidManifest = File('android/app/src/main/AndroidManifest.xml');
-  if (androidManifest.existsSync()) {
-    String manifestContent = androidManifest.readAsStringSync();
-    final labelPattern = RegExp(r'android:label="[^"]*"');
-    if (labelPattern.hasMatch(manifestContent)) {
-      manifestContent = manifestContent.replaceAll(
-          labelPattern, 'android:label="@string/app_name"');
+    // Update AndroidManifest.xml with android:label="@string/app_name"
+    final androidManifest = File('android/app/src/main/AndroidManifest.xml');
+    if (androidManifest.existsSync()) {
+      String manifestContent = androidManifest.readAsStringSync();
+      final labelPattern = RegExp(r'android:label="[^"]*"');
+      if (labelPattern.hasMatch(manifestContent)) {
+        manifestContent = manifestContent.replaceAll(
+            labelPattern, 'android:label="@string/app_name"');
+      }
+
+      androidManifest.writeAsStringSync(manifestContent);
+      print(
+          'AndroidManifest.xml updated with android:label="@string/app_name".');
+    } else {
+      print('AndroidManifest.xml not found.');
     }
-
-    androidManifest.writeAsStringSync(manifestContent);
-    print('AndroidManifest.xml updated with android:label="@string/app_name".');
-  } else {
-    print('AndroidManifest.xml not found.');
   }
-
-  // Generate iOS InfoPlist.strings files
-  locales.forEach((locale, name) {
-    final dir = Directory('ios/Runner/$locale.lproj');
-    if (!dir.existsSync()) {
-      dir.createSync(recursive: true);
-    }
-    final file = File('${dir.path}/InfoPlist.strings');
-    file.writeAsStringSync('''
+  if (platforms['ios'] == true) {
+    // Generate iOS InfoPlist.strings files
+    locales.forEach((locale, name) {
+      final dir = Directory('ios/Runner/$locale.lproj');
+      if (!dir.existsSync()) {
+        dir.createSync(recursive: true);
+      }
+      final file = File('${dir.path}/InfoPlist.strings');
+      file.writeAsStringSync('''
 "CFBundleDisplayName" = "${name ?? defaultName}";
     ''');
-  });
-
+    });
+  }
   print('Localized app name files generated successfully.');
 }
